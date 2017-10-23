779c62a5a8942de85b105dbaa96174d0e0564a85
Liam Dwyer <ljd3za@virginia.edu>
2017-10-23 17:30:56 -0400

    Added basic left and right spell and combine spell functionality

--- The Library/Assets/SpellManagementScript.cs
+++ The Library/Assets/SpellManagementScript.cs
@@ -1,69 +1,137 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
 public class SpellManagementScript : MonoBehaviour {
     
     public GameObject[] Prefabs;
+    public Dictionary<string, GameObject> combinedSpells = new Dictionary<string, GameObject>();
 
     private GameObject currentPrefabObject;
-    private int currentPrefabIndex;
+    private GameObject combinedEffect;
+    private int currentPrefabIndexRight;
+    private int currentPrefabIndexLeft;
+    private bool combinedModeEntered = false;
+    
 
     private void UpdateEffect()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            StartCurrent();
+            StartCurrentLeft();
         }
-        else if (Input.GetKeyDown(KeyCode.Plus) || Input.GetKeyDown(KeyCode.KeypadPlus))
+        else if (Input.GetMouseButtonDown(1))
         {
-            NextPrefab();
+            StartCurrentRight();
         }
-        else if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus))
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            PreviousPrefab();
+            NextPrefabRight();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            PreviousPrefabRight();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            NextPrefabLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            PreviousPrefabLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CombineEffects(currentPrefabIndexLeft, currentPrefabIndexRight);
         }
     }
 
-    void BeginEffect() {
-        currentPrefabObject = GameObject.Instantiate(Prefabs[currentPrefabIndex]);
+    void Start()
+    {
+        // Temporary proof of concept
+        string[] testSpells = new string[] { Prefabs[0].name, Prefabs[1].name };
+        Array.Sort(testSpells);
+        string key = testSpells[0] + ", " + testSpells[1];
+        combinedSpells.Add(key, Prefabs[2]);
     }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
         UpdateEffect();
 	}
 
-    public void StartCurrent()
+    public void StartCurrentRight()
     {
         StopCurrent();
-        BeginEffect();
+        Vector3 right_pos = new Vector3(transform.position.x - 10, transform.position.y, transform.position.z);
+        currentPrefabObject = GameObject.Instantiate(Prefabs[currentPrefabIndexRight]);
+        //currentPrefabObject.transform.TransformDirection(right_pos);
+    }
+    public void StartCurrentLeft()
+    {
+        StopCurrent();
+        if (combinedModeEntered)
+        {
+            currentPrefabObject = GameObject.Instantiate(combinedEffect);
+            combinedModeEntered = false;
+        }
+        else
+        {
+            currentPrefabObject = GameObject.Instantiate(Prefabs[currentPrefabIndexLeft]);
+        }
     }
     public void StopCurrent()
     {
         currentPrefabObject = null;
     }
 
-    public void NextPrefab()
+    public void NextPrefabRight()
     {
-        currentPrefabIndex++;
-        if (currentPrefabIndex == Prefabs.Length)
+        currentPrefabIndexRight++;
+        if (currentPrefabIndexRight == Prefabs.Length-1)
         {
-            currentPrefabIndex = 0;
+            currentPrefabIndexRight = 0;
         }
     }
 
-    public void PreviousPrefab()
+    public void PreviousPrefabRight()
     {
-        currentPrefabIndex--;
-        if (currentPrefabIndex == -1)
+        currentPrefabIndexRight--;
+        if (currentPrefabIndexRight == -1)
         {
-            currentPrefabIndex = Prefabs.Length - 1;
+            currentPrefabIndexRight = Prefabs.Length - 1;
+        }
+    }
+
+    public void NextPrefabLeft()
+    {
+        currentPrefabIndexLeft++;
+        if (currentPrefabIndexLeft == Prefabs.Length-1)
+        {
+            currentPrefabIndexLeft = 0;
+        }
+    }
+
+    public void PreviousPrefabLeft()
+    {
+        currentPrefabIndexLeft--;
+        if (currentPrefabIndexLeft == -1)
+        {
+            currentPrefabIndexLeft = Prefabs.Length - 1;
+        }
+    }
+
+    public void CombineEffects(int index1, int index2)
+    {
+        string[] effects = new string[] { Prefabs[index1].name, Prefabs[index2].name };
+        Array.Sort(effects);
+        string key = effects[0] + ", " + effects[1];
+        if (combinedSpells.ContainsKey(key))
+        {
+            combinedModeEntered = true;
+            combinedEffect = combinedSpells[key];
         }
     }
 }
