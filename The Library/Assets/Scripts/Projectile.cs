391e79e98e939e348ab348f1d5189ab120158511
Liam Dwyer <ljd3za@virginia.edu>
2017-11-08 23:08:30 -0500

    Actually mapped VR controls

--- The Library/Assets/Scripts/Projectile.cs
+++ The Library/Assets/Scripts/Projectile.cs
@@ -6,12 +6,12 @@ public class Projectile : MonoBehaviour {
 
     public float speed = 5.0f;
     public float duration = 2.0f;
-    protected Vector3 direction;
+    public Vector3 direction;
     private float initializationTime;
 
 	// Use this for initialization
 	void Start () {
-        var rot = Quaternion.Euler(Camera.main.transform.eulerAngles);
+        var rot = Quaternion.Euler(direction);
         var yangle = rot * Vector3.forward;
         direction = yangle * speed;
         initializationTime = Time.timeSinceLevelLoad;
