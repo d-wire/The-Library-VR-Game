91c24f48fff2ad386f12e42dbcf18daef5fce2b1
mpt5nm <mpt5nm@virginia.edu>
2017-11-09 20:25:13 -0500

    Added objects in room

---
+++ The Library/Assets/Prefab/Script/WaterProjectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterProjectile : Projectile
+{
+    protected override void handleCollision(Collider other)
+    {
+        if (other.tag == "Structure")
+        {
+            Destroy(gameObject);
+        }
+        else if (other.tag == "Object" && other.gameObject.GetComponent<ObjectScript>().fillable)
+        {
+            Destroy(gameObject);
+            other.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
