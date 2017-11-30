0f3f0cb32338bb229e7834bd7ba9b459e24415d6
Liam Dwyer <ljd3za@virginia.edu>
2017-11-29 22:34:49 -0500

    Styled the level

---
+++ The Library/Assets/TutorialMangerPart2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialMangerPart2 : MonoBehaviour {
+
+    public Text tutorialText;
+    public SteamVR_TrackedController _controller1;
+    public SteamVR_TrackedController _controller2;
+
+    // Use this for initialization
+
+    private void OnEnable()
+    {
+        // _controller1 = GetComponent<SteamVR_TrackedController>();
+        _controller1.TriggerClicked += HandleTriggerClicked;
+        _controller1.PadClicked += HandlePadClicked;
+
+        // _controller2 = GetComponent<SteamVR_TrackedController>();
+        _controller2.TriggerClicked += HandleTriggerClicked;
+        _controller2.PadClicked += HandlePadClicked;
+    }
+
+    private void HandleTriggerClicked(object sender, ClickedEventArgs e)
+    {
+
+    }
+
+    private void HandlePadClicked(object sender, ClickedEventArgs e)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
